c077ca3c1f835cfb86f7f06178cc11fd92846884
Mehdi Maatouk <mehdysnt5@hotmail.fr>
2019-04-26 12:12:41 +0200

    First commit

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return(
+      <div>
+      <h1>Question</h1>
+
+      </div>
+    )
+  }
+}
+
+export default Main
